e37ed855013fde75319c340797ac0fb747df0b1b
YG <710431075@qq.com>
2022-10-28 12:04:36 +0800

    Initial commit

---
+++ netty/src/test/java/cn/itcast/netty/c1/HelloClient.java
@@ -0,0 +1,39 @@
+package cn.itcast.netty.c1;
+
+/*
+@author YG
+@create 2022/10/28   11:47
+*/
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        // 1. 启动类
+        new Bootstrap()
+                // 2. 添加 EventLoop，接收服务器的数据、处理
+                .group(new NioEventLoopGroup())
+                // 3. 选择客户端 channel 实现
+                .channel(NioSocketChannel.class)
+                // 4. 添加处理器
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override // 在连接建立后，初始化
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        // 添加编码器
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                // 5. 连接到服务器
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync()
+                .channel()
+                // 6. 向服务器发送数据
+                .writeAndFlush("hello, world");
+    }
+}
